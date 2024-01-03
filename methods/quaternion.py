24310a5dd9c946844069cd27232914db6a119c15
cq <chenqink@outlook.com>
2024-01-03 16:18:53 +0800

    add doc

--- methods/quaternion.py
+++ methods/quaternion.py
@@ -9,36 +9,20 @@ class QuaternionPointCloudProcessor(BasePointCloudProcessor):
 		print(f'Now doing ICP with quaternion method. Sampling mode: {sampling_mode}')
 		super().__init__(save_dir='result/quaternion/', sampling_mode=sampling_mode)
 
-	# def icp_core_method1(self, pts1, pts2, latest_transformation=None):
-	# 	pts1 = pts1[:, :3]
-	# 	pts2 = pts2[:, :3]
-	# 	row, col = pts1.shape
-	# 	mean_template_pts = np.mean(pts1, axis=0)
-	# 	mean_register_pts = np.mean(pts2, axis=0)
-	# 	cov = np.matmul((pts2 - mean_register_pts).T, (pts1 - mean_template_pts)) / row
-	# 	a_matrix = cov - cov.T
-	# 	delta = np.array([a_matrix[1, 2], a_matrix[2, 0], a_matrix[0, 1]], dtype=np.float32).T
-	# 	q_matrix = np.zeros((4, 4), dtype=np.float32)
-	# 	q_matrix[0, 0] = np.trace(cov)
-	# 	q_matrix[0, 1:] = delta
-	# 	q_matrix[1:, 0] = delta
-	# 	q_matrix[1:, 1:] = cov + cov.T - np.trace(cov) * np.eye(3)
-	# 	lambdas, eigen_vectors = np.linalg.eig(q_matrix)
-	# 	q = eigen_vectors[:, np.argmax(lambdas)]
-	# 	rotation_matrix = quaternion_to_rotation_matrix(q)
-	# 	translation_matrix = mean_template_pts - np.matmul(rotation_matrix, mean_register_pts)
-	# 	registered_points = np.matmul(
-	# 		rotation_matrix, pts2.T).T + translation_matrix
-	# 	error = np.mean(
-	# 		np.sqrt(np.sum(np.square(registered_points - pts1), axis=1)))
-
-	# 	new_transform = np.zeros((4, 4))
-	# 	new_transform[0:3, 0:3] = rotation_matrix
-	# 	new_transform[:3, 3] = translation_matrix.T
-	# 	new_transform[3, 3] = 1
-	# 	return new_transform, error
-		
 	def icp_core_method(self, pts1, pts2, latest_transformation=None):
+		"""
+		Perform the core method of the Iterative Closest Point (ICP) algorithm.
+
+		Args:
+			pts1 (numpy.ndarray): Array of 3D points representing the source point cloud.
+			pts2 (numpy.ndarray): Array of 3D points representing the target point cloud.
+			latest_transformation (numpy.ndarray, optional): Latest transformation matrix. Defaults to None.
+
+		Returns:
+			numpy.ndarray: The new transformation matrix.
+			float: The alignment error.
+
+		"""
 		# Select only the first three columns (x, y, z) from pts1 and pts2
 		pts1, pts2 = pts1[:, :3], pts2[:, :3]
 
--- methods/svd.py
+++ methods/svd.py
@@ -8,37 +8,23 @@ class SVDPointCloudProcessor(BasePointCloudProcessor):
 		print(f'Now doing ICP with SVD method. Sampling mode: {sampling_mode}')
 		super().__init__(save_dir='result/svd/', sampling_mode=sampling_mode)
 
-	# def icp_core_method(self, pts1, pts2, latest_transformation=None):
-	# 	pts1 = pts1[:, :3]
-	# 	pts2 = pts2[:, :3]
-	# 	row, col = pts1.shape
-	# 	weights = np.eye(row)
-	# 	p = pts2.T
-	# 	q = pts1.T
-	# 	mean_p = np.dot(p, np.diagonal(
-	# 		weights).reshape(-1, 1)) / np.trace(weights)
-	# 	mean_q = np.dot(q, np.diagonal(
-	# 		weights).reshape(-1, 1)) / np.trace(weights)
-	# 	x_matrix = p - mean_p
-	# 	y_matrix = q - mean_q
-	# 	s_matrix = np.matmul(np.matmul(x_matrix, weights), y_matrix.T)
-	# 	u_matrix, sigma, v_matrix = np.linalg.svd(s_matrix)
-	# 	det_v_ut = np.linalg.det(np.matmul(v_matrix.T, u_matrix.T))
-	# 	diag_matrix = np.eye(3)
-	# 	diag_matrix[2, 2] = det_v_ut
-	# 	rotation_matrix = np.matmul(np.matmul(v_matrix.T, diag_matrix), u_matrix.T)
-	# 	translation_matrix = mean_q - np.matmul(rotation_matrix, mean_p)
-	# 	registered_pts = np.matmul(
-	# 		rotation_matrix, pts2.T) + translation_matrix
-	# 	error = np.mean(
-	# 		np.sqrt(np.sum(np.square(registered_pts.T - pts1), axis=1)))
-	# 	new_transform = np.zeros((4, 4))
-	# 	new_transform[0:3, 0:3] = rotation_matrix
-	# 	new_transform[:3, 3] = translation_matrix.T
-	# 	new_transform[3, 3] = 1
+	def icp_core_method(self, pts1, pts2, latest_transformation=None):
+		"""
+		Perform the Iterative Closest Point (ICP) core method to align two sets of 3D points.
 
-	# 	return new_transform, error
+		Args:
+			pts1 (numpy.ndarray): Array of shape (N, 3) representing the first set of 3D points.
+			pts2 (numpy.ndarray): Array of shape (N, 3) representing the second set of 3D points.
+			latest_transformation (numpy.ndarray, optional): Transformation matrix from the previous iteration. Defaults to None.
 
+		Returns:
+			tuple: A tuple containing the new transformation matrix and the mean squared error.
+
+		"""
+		# Use only the x, y, z coordinates
+		pts1, pts2 = pts1[:, :3], pts2[:, :3]
+
+		# Rest of the code...
 	def icp_core_method(self, pts1, pts2, latest_transformation=None):
 		# Use only the x, y, z coordinates
 		pts1, pts2 = pts1[:, :3], pts2[:, :3]
